e2768733517a36826f6153dc37330140e61ed97e
danielsoftdeleloper <daniel.andrade@sga.pucminas.br>
2021-02-03 18:12:55 -0300

    Embaralha e associa imagen ao clique.

--- jogo-da-memória/memoria.js
+++ jogo-da-memória/memoria.js
@@ -5,34 +5,65 @@ let fundo = "https://picsum.photos/100?grayscale";
 
 // Estado do jogo
 let cartas = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8];
+let cliquesTravados = false;
 
 onload = () => {
-    /* carrega as imagens de fundo */
-  let elementosImagens = document.querySelectorAll("#memoria img");
-  elementosImagens.forEach((img, i) =>{
-      img.src =fundo;
-      img.setAttribute('data-valor', i);
-      img.style.opacity = 0.5;
+  /* carrega as imagens de fundo */
+  let elemeImagens = document.querySelectorAll("#memoria img");
+  elemeImagens.forEach((img, i) => {
+    img.src = fundo;
+    img.setAttribute("data-valor", i);
+    img.style.opacity = 0.4;
   });
 
   /* Cria o evento do botão de Inicio */
-  document.querySelector('#bt-inicio').onclik = iniciaJogo;
-
+  document.querySelector("#btInicio").onclick = iniciaJogo;
 };
 
-  // ====================================
-  // Inicia o Jogo da memoria.
-  // ====================================
+// ====================================
+// Inicia o Jogo da memoria.
+// ====================================
 
-  const iniciaJogo = () =>{
+const iniciaJogo = () => {
+  /* console.log("cartasIniciais", cartas); */
 
-    // Primeiro embaralhar as cartas
-    for(let i = 0; i < cartas.length; i++){
-        let p = Math.trunc( Math.random() * cartas.length);
-        let aux = cartas[p];
-        cartas[p] = cartas[i];
-        cartas[i] = aux;
-    }
-    console.log(cartas);
-
+  // Primeiro embaralhar as cartas
+  for (let i = 0; i < cartas.length; i++) {
+    let p = Math.trunc(Math.random() * cartas.length);
+    let aux = cartas[p];
+    cartas[p] = cartas[i];
+    cartas[i] = aux;
   }
+  /*  console.log("cartasEmbaralhadas", cartas); */
+
+  // associar evento as imagens
+  let elemeImagens = document.querySelectorAll("#memoria img");
+  elemeImagens.forEach((img, i) => {
+    img.onclick = trataCliqueImagem;
+    img.style.opacity = 1;
+    /*  img.src = fundo; */
+  });
+};
+
+// ====================================
+// Processa a imagem
+// ====================================
+
+const trataCliqueImagem = (e) => {
+  
+
+  // + muda string na number.
+  const p = +e.target.getAttribute("data-valor");
+  const valor = cartas[p]; //16 cartas
+  e.target.src = imagens[valor - 1];
+  e.target.onclick = null;
+
+  cliquesTravados = true;
+
+
+  setTimeout(() => {
+    e.target.src = fundo;
+    e.target.onclick = trataCliqueImagem;
+    cliquesTravados = false;
+  }, 2500);
+};
