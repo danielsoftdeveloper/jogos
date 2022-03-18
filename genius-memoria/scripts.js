d10c5a67a954470624779b7873a64491b6c6e2ca
danielsoftdeveloper <daniel.andrade@sga.pucminas.br>
2022-03-18 16:29:53 -0300

    cria a ordem aleatória das cores

--- genius-memoria/scripts.js
+++ genius-memoria/scripts.js
@@ -1,1 +1,28 @@
-console.log("Hello world");
+let order = [];
+let clickedOrder = [];
+let score = 0;
+
+//0 - verde
+//1 - vermelho
+//2 - amarelo
+//3 - azul
+
+const green = document.querySelector('.green');
+
+const red  = document.querySelector('.red');
+
+const blue = document.querySelector('.blue');
+
+const yellow = document.querySelector('.yellow');
+
+//cria a ordem aleatória das cores
+let SorteiaOrder = () => {
+    let colorOrder = Math.floor(Math.random() * 4);
+    order[order.length] = colorOrder;
+    clickedOrder = [];
+
+    for(let i in order) {
+        let elementColor = createColorElement(order[i]);
+        lightColor(elementColor, Number(i) + 1);
+    }
+}
